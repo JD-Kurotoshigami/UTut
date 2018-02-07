3b3078b39f1fbf4c56f9ab4fe7ea42f2330db811
jcsegismundo <julessegismundo@gmail.com>
2018-02-07 13:44:43 +0800

    User is now logged in after sign up. /users url now redirects to home. Fixed sign up password field. /user/:id redirects to home if :id does not exist in the database.

--- Code/UTut_University_Tutor/app/controllers/users_controller.rb
+++ Code/UTut_University_Tutor/app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    redirect_to root_url
+  end
   def show
-    @user = User.find(params[:id])
-    
+    if User.exists?(params[:id])
+      @user = User.find(params[:id])
+    else
+      redirect_to root_url
+    end
   end
 
   def new
@@ -10,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
