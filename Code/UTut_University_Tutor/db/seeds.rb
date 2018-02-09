56e49fa1fd65fc2436bad7769aa15ba822a5a16e
jcsegismundo <julessegismundo@gmail.com>
2018-02-09 12:33:19 +0800

    Added sprint retrospect documents in Sprint 1 folder under 01 - Project Documents. Note: Mendoza Sprint Retrospect contains the group review.

--- Code/UTut_University_Tutor/db/seeds.rb
+++ Code/UTut_University_Tutor/db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.new(username: 'cs145xyhonor', firstname: 'CS 145', lastname: 'MXY HONOR', sex: 'Male', password: 'mantika2log', password_confirmation: 'mantika2log')
-User.new(username: 'event1', firstname: 'Event', lastname: 'Juan', sex: 'Male', password: 'criticalisaw', password_confirmation: 'critical_isaw')
-User.new(username: 'cs165htuv_luntian', firstname: 'One Six Five', lastname: 'Luntian', sex: 'Female', password: 'helicop1', password_confirmation: 'helicop1')
+@user = User.new(username: 'cs145xyhonor', firstname: 'CS 145', lastname: 'MXY HONOR', sex: 'Male', password: 'mantika2log', password_confirmation: 'mantika2log')
+@user.save
+@user = User.new(username: 'event1', firstname: 'Event', lastname: 'Juan', sex: 'Male', password: 'criticalisaw', password_confirmation: 'critical_isaw')
+@user.save
+@user = User.new(username: 'cs165htuv_luntian', firstname: 'One Six Five', lastname: 'Luntian', sex: 'Female', password: 'helicop1', password_confirmation: 'helicop1')
+@user.save
