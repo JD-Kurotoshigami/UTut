8f1f0f0dff43afc019936fca27ee11056e766786
jcsegismundo <julessegismundo@gmail.com>
2018-03-07 12:34:34 +0800

    fixed an error in sessionshelper

--- Code/UTut_University_Tutor/app/helpers/sessions_helper.rb
+++ Code/UTut_University_Tutor/app/helpers/sessions_helper.rb
@@ -38,7 +38,7 @@ module SessionsHelper
      # Attempts to log in a user
      # User: The user to log in
      def log_in(user)
-          sessiongh[:username] = user.username
+          session[:username] = user.username
           session[:error] = nil
      end
 
